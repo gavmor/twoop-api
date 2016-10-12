d8e71bc802e304aff8c3bad822c890a8d49db061
Gavin Morgan <quavmo@gmail.com>
2016-10-11 22:33:46 -0700

    serves twax

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-import { taxonomize as twax } from 'twax';
+import dotenv from 'dotenv';
+import Twax from 'twax';
+import koa from 'koa';
+import koaPath from 'koa-path';
+
+dotenv.config();
+
+const app = koa();
+const route = koaPath();
+const twax = new Twax();
+
+app.use(route('/:handle', function *(){
+  yield twax.taxonomize(this.params.handle)
+            .then(body => Object.assign(this, {body}));
+}));
+
+// app.use(function *(){
+//   console.log(arguments)
+//   return twax.taxonomize('quavmo').then(body => Object.assign(this, {body}));
+// });
+
+console.log('listening');
+app.listen(3000);
