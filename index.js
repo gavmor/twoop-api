b967d0b9de1e3eafffeae1d5e0e6dff2915981b3
Gavin Morgan <quavmo@gmail.com>
2016-10-11 22:40:03 -0700

    compile for heroku

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-import dotenv from 'dotenv';
-import Twax from 'twax';
-import koa from 'koa';
-import koaPath from 'koa-path';
+'use strict';
 
-dotenv.config();
+var _dotenv = require('dotenv');
 
-const app = koa();
-const route = koaPath();
-const twax = new Twax();
+var _dotenv2 = _interopRequireDefault(_dotenv);
 
-app.use(route('/:handle', function *(){
-  yield twax.taxonomize(this.params.handle)
-            .then(body => Object.assign(this, {body}));
-}));
+var _twax = require('twax');
+
+var _twax2 = _interopRequireDefault(_twax);
+
+var _koa = require('koa');
+
+var _koa2 = _interopRequireDefault(_koa);
+
+var _koaPath = require('koa-path');
+
+var _koaPath2 = _interopRequireDefault(_koaPath);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_dotenv2.default.config();
+
+var app = (0, _koa2.default)();
+var route = (0, _koaPath2.default)();
+var twax = new _twax2.default();
+
+app.use(route('/:handle', regeneratorRuntime.mark(function _callee() {
+  var _this = this;
+
+  return regeneratorRuntime.wrap(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.next = 2;
+          return twax.taxonomize(this.params.handle).then(function (body) {
+            return Object.assign(_this, { body: body });
+          });
+
+        case 2:
+        case 'end':
+          return _context.stop();
+      }
+    }
+  }, _callee, this);
+})));
 
 // app.use(function *(){
 //   console.log(arguments)
@@ -20,4 +50,4 @@ app.use(route('/:handle', function *(){
 // });
 
 console.log('listening');
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
---
+++ src.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import Twax from 'twax';
+import koa from 'koa';
+import koaPath from 'koa-path';
+
+dotenv.config();
+
+const app = koa();
+const route = koaPath();
+const twax = new Twax();
+
+app.use(route('/:handle', function *(){
+  yield twax.taxonomize(this.params.handle)
+            .then(body => Object.assign(this, {body}));
+}));
+
+console.log('listening');
+app.listen(process.env.PORT || 3000);
