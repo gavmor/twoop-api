14151e284b36bdc24f3e4d697be701da5dc81b39
Gavin Morgan <quavmo@gmail.com>
2016-10-25 15:37:49 -0700

    two paths: screen_name,user_id

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _babelPolyfill = require('babel-polyfill');
 
 var _babelPolyfill2 = _interopRequireDefault(_babelPolyfill);
@@ -20,6 +22,14 @@ var _koaPath = require('koa-path');
 
 var _koaPath2 = _interopRequireDefault(_koaPath);
 
+var _koaCors = require('koa-cors');
+
+var _koaCors2 = _interopRequireDefault(_koaCors);
+
+var _koaLogger = require('koa-logger');
+
+var _koaLogger2 = _interopRequireDefault(_koaLogger);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _dotenv2.default.config();
@@ -28,7 +38,10 @@ var app = (0, _koa2.default)();
 var route = (0, _koaPath2.default)();
 var twax = new _twax2.default();
 
-app.use(route('/:handle', regeneratorRuntime.mark(function _callee() {
+app.use((0, _koaCors2.default)());
+app.use((0, _koaLogger2.default)());
+
+app.use(route('/user_id/:user_id', regeneratorRuntime.mark(function _callee() {
   var _this = this;
 
   return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -36,7 +49,7 @@ app.use(route('/:handle', regeneratorRuntime.mark(function _callee() {
       switch (_context.prev = _context.next) {
         case 0:
           _context.next = 2;
-          return twax.taxonomize(this.params.handle).then(function (body) {
+          return twax.taxonomize(_extends({}, this.params)).then(function (body) {
             return Object.assign(_this, { body: body });
           });
 
@@ -46,6 +59,27 @@ app.use(route('/:handle', regeneratorRuntime.mark(function _callee() {
       }
     }
   }, _callee, this);
+})));
+
+app.use(route('/screen_name/:screen_name', regeneratorRuntime.mark(function _callee2() {
+  var _this2 = this;
+
+  return regeneratorRuntime.wrap(function _callee2$(_context2) {
+    while (1) {
+      switch (_context2.prev = _context2.next) {
+        case 0:
+          console.log(this.params);
+          _context2.next = 3;
+          return twax.taxonomize(_extends({}, this.params)).then(function (body) {
+            return Object.assign(_this2, { body: body });
+          });
+
+        case 3:
+        case 'end':
+          return _context2.stop();
+      }
+    }
+  }, _callee2, this);
 })));
 
 console.log('listening');
--- src.js
+++ src.js
@@ -2,16 +2,27 @@ import babelPolyfill from 'babel-polyfill';
 import dotenv from 'dotenv';
 import Twax from 'twax';
 import koa from 'koa';
-import koaPath from 'koa-path';
+import path from 'koa-path';
+import cors from 'koa-cors';
+import logger from 'koa-logger';
 
 dotenv.config();
 
 const app = koa();
-const route = koaPath();
+const route = path();
 const twax = new Twax();
 
-app.use(route('/:handle', function *(){
-  yield twax.taxonomize(this.params.handle)
+app.use(cors());
+app.use(logger());
+
+app.use(route('/user_id/:user_id', function *() {
+  yield twax.taxonomize({...this.params})
+            .then(body => Object.assign(this, {body}));
+}));
+
+app.use(route('/screen_name/:screen_name', function *() {
+  console.log(this.params);
+  yield twax.taxonomize({...this.params})
             .then(body => Object.assign(this, {body}));
 }));
 
